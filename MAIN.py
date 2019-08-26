4f98b690635af5ed3aae8a0a9194c37fa3ae6836
z404 <anishr890@gmail.com>
2019-08-25 20:16:59 -0400

    final

--- MAIN.py
+++ MAIN.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 import datetime,math
 import tkinter as tk
+from tkinter import messagebox
 
 root = tk.Tk()
 
@@ -18,106 +19,111 @@ def clear_window(window):
 
 def predict(company_name, days_to_predict):
     style.use('ggplot')
-    df= pd.read_csv("GOOG.csv", header=0, index_col='Date', parse_dates=True)
-    #df = quandl.get("WIKI/GOOGL")
-    df = df[['Open',  'High',  'Low',  'Adj Close', 'Volume']]
-    df['HL_PCT'] = (df['High'] - df['Low']) / df['Adj Close'] * 100.0
-    df['PCT_change'] = (df['Adj Close'] - df['Open']) / df['Open'] * 100.0
+    try:
+        df= pd.read_csv(company_name+".csv", header=0, index_col='Date', parse_dates=True)
 
-    df = df[['Adj Close', 'HL_PCT', 'PCT_change', 'Volume']]
-    forecast_col = 'Adj Close'
-    df.fillna(value=-99999, inplace=True)
-    #forecast_out = int(math.ceil(0.1 * len(df)))
-    forecast_out = days_to_predict
-    df['label'] = df[forecast_col].shift(-forecast_out)
+        #df = quandl.get("WIKI/GOOGL")
+        df = df[['Open',  'High',  'Low',  'Adj Close', 'Volume']]
+        df['HL_PCT'] = (df['High'] - df['Low']) / df['Adj Close'] * 100.0
+        df['PCT_change'] = (df['Adj Close'] - df['Open']) / df['Open'] * 100.0
 
-    X = np.array(df.drop(['label'], 1))
-    X = preprocessing.scale(X)
-    X_lately = X[-forecast_out:]
-    X = X[:-forecast_out]
+        df = df[['Adj Close', 'HL_PCT', 'PCT_change', 'Volume']]
+        forecast_col = 'Adj Close'
+        df.fillna(value=-99999, inplace=True)
+        #forecast_out = int(math.ceil(0.1 * len(df)))
+        forecast_out = days_to_predict
+        df['label'] = df[forecast_col].shift(-forecast_out)
 
-    df.dropna(inplace=True)
+        X = np.array(df.drop(['label'], 1))
+        X = preprocessing.scale(X)
+        X_lately = X[-forecast_out:]
+        X = X[:-forecast_out]
 
-    y = np.array(df['label'])
+        df.dropna(inplace=True)
 
-    X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
-    clf = svm.SVR(kernel='linear')
-    clf.fit(X_train, y_train)
-    confidence = clf.score(X_test, y_test)
-    print(confidence)
+        y = np.array(df['label'])
+
+        X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
+        clf = svm.SVR(kernel='linear')
+        clf.fit(X_train, y_train)
+        confidence = clf.score(X_test, y_test)
+        print(confidence)
 
 
-    forecast_set = clf.predict(X_lately)
-    df['Forecast'] = np.nan
+        forecast_set = clf.predict(X_lately)
+        df['Forecast'] = np.nan
 
-    last_date = df.iloc[-1].name
-    last_unix = last_date.timestamp()
-    one_day = 86400
-    next_unix = last_unix + one_day
+        last_date = df.iloc[-1].name
+        last_unix = last_date.timestamp()
+        one_day = 86400
+        next_unix = last_unix + one_day
 
-    for i in forecast_set:
-        next_date = datetime.datetime.fromtimestamp(next_unix)
-        next_unix += 86400
-        df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)]+[i]
+        for i in forecast_set:
+            next_date = datetime.datetime.fromtimestamp(next_unix)
+            next_unix += 86400
+            df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)]+[i]
 
-    check_difference = df['Adj Close'][-days_to_predict-1] - df['Forecast'][len(df[0:-days_to_predict])]
-    df['Forecast'] = df['Forecast']+check_difference
-        
-    df['Adj Close'].plot()
-    df['Forecast'].plot()
-    plt.legend(loc=4)
-    plt.xlabel('Date')
-    plt.ylabel('Price')
-    plt.show()
+        check_difference = df['Adj Close'][-days_to_predict-1] - df['Forecast'][len(df[0:-days_to_predict])]
+        df['Forecast'] = df['Forecast']+check_difference
+            
+        df['Adj Close'].plot()
+        df['Forecast'].plot()
+        plt.legend(loc=4)
+        plt.xlabel('Date')
+        plt.ylabel('Price')
+        plt.show()
 
-##    style.use('ggplot')
-##    df= pd.read_csv("GOOG.csv", header=0, index_col='Date', parse_dates=True)
-##    df = df[['Open',  'High',  'Low',  'Adj Close', 'Volume']]
-##    df['HL_PCT'] = (df['High'] - df['Low']) / df['Adj Close'] * 100.0
-##    df['PCT_change'] = (df['Adj Close'] - df['Open']) / df['Open'] * 100.0
-##    df = df[['Adj Close', 'HL_PCT', 'PCT_change', 'Volume']]
-##    forecast_col = 'Adj Close'
-##    df.fillna(value=-99999, inplace=True)
-##    forecast_out = days_to_predict
-##    df['label'] = df[forecast_col].shift(-forecast_out)
-##    X = np.array(df.drop(['label'], 1))
-##    #print(X)
-##    X = preprocessing.scale(X)
-##    print(X)
-##    X_lately = X[-forecast_out:]
-##    X = X[:-forecast_out]
-##
-##    df.dropna(inplace=True)
-##
-##    y = np.array(df['label'])
-##
-##    X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
-##    clf = LinearRegression(n_jobs=-1)
-##    clf.fit(X_train, y_train)
-##    confidence = clf.score(X_test, y_test)
-##
-##    forecast_set = clf.predict(X_lately)
-##    df['Forecast'] = np.nan
-##
-##    last_date = df.iloc[-1].name
-##    last_unix = last_date.timestamp()
-##    one_day = 86400
-##    next_unix = last_unix + one_day
-##
-##    for i in forecast_set:
-##        next_date = datetime.datetime.fromtimestamp(next_unix)
-##        next_unix += 86400
-##        df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)]+[i]
-##
-##    check_difference = df['Adj Close'][-days_to_predict-1] - df['Forecast'][len(df[0:-days_to_predict])]
-##    df['Forecast'] = df['Forecast']+check_difference
-##    
-##    df['Adj Close'].plot()
-##    df['Forecast'].plot()
-##    plt.legend(loc=4)
-##    plt.xlabel('Date')
-##    plt.ylabel('Price')
-##    plt.show()
+    ##    style.use('ggplot')
+    ##    df= pd.read_csv("GOOG.csv", header=0, index_col='Date', parse_dates=True)
+    ##    df = df[['Open',  'High',  'Low',  'Adj Close', 'Volume']]
+    ##    df['HL_PCT'] = (df['High'] - df['Low']) / df['Adj Close'] * 100.0
+    ##    df['PCT_change'] = (df['Adj Close'] - df['Open']) / df['Open'] * 100.0
+    ##    df = df[['Adj Close', 'HL_PCT', 'PCT_change', 'Volume']]
+    ##    forecast_col = 'Adj Close'
+    ##    df.fillna(value=-99999, inplace=True)
+    ##    forecast_out = days_to_predict
+    ##    df['label'] = df[forecast_col].shift(-forecast_out)
+    ##    X = np.array(df.drop(['label'], 1))
+    ##    #print(X)
+    ##    X = preprocessing.scale(X)
+    ##    print(X)
+    ##    X_lately = X[-forecast_out:]
+    ##    X = X[:-forecast_out]
+    ##
+    ##    df.dropna(inplace=True)
+    ##
+    ##    y = np.array(df['label'])
+    ##
+    ##    X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
+    ##    clf = LinearRegression(n_jobs=-1)
+    ##    clf.fit(X_train, y_train)
+    ##    confidence = clf.score(X_test, y_test)
+    ##
+    ##    forecast_set = clf.predict(X_lately)
+    ##    df['Forecast'] = np.nan
+    ##
+    ##    last_date = df.iloc[-1].name
+    ##    last_unix = last_date.timestamp()
+    ##    one_day = 86400
+    ##    next_unix = last_unix + one_day
+    ##
+    ##    for i in forecast_set:
+    ##        next_date = datetime.datetime.fromtimestamp(next_unix)
+    ##        next_unix += 86400
+    ##        df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)]+[i]
+    ##
+    ##    check_difference = df['Adj Close'][-days_to_predict-1] - df['Forecast'][len(df[0:-days_to_predict])]
+    ##    df['Forecast'] = df['Forecast']+check_difference
+    ##    
+    ##    df['Adj Close'].plot()
+    ##    df['Forecast'].plot()
+    ##    plt.legend(loc=4)
+    ##    plt.xlabel('Date')
+    ##    plt.ylabel('Price')
+    ##    plt.show()
+    except FileNotFoundError:
+        messagebox.showerror(title='Data not found',message='The data for the company listed was not found! Please download the data from finance.google.com!')
+    
     
 def start_menu():
     global root
